c13dea3259c85a489cf607484896638bf1769bbf
izzaafkarina14 <122216304+izzaafkarina14@users.noreply.github.com>
2024-09-05 18:17:27 +0700

    Create Day 23: BST Level-Order Traversal.js

---
+++ Day 23: BST Level-Order Traversal.js
@@ -0,0 +1,85 @@
+// Start of function Node
+function Node(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+}; // End of function Node
+
+// Start of function BinarySearchTree
+function BinarySearchTree() {
+    this.insert = function(root, data) {
+        if (root === null) {
+            this.root = new Node(data);
+            
+            return this.root;
+        }
+        
+        if (data <= root.data) {
+            if (root.left) {
+                this.insert(root.left, data);
+            } else {
+                root.left = new Node(data);
+            }
+        } else {
+            if (root.right) {
+                this.insert(root.right, data);
+            } else {
+                root.right = new Node(data);
+            }
+        }
+        
+        return this.root;
+    };
+    
+    // Start of function levelOrder
+    this.levelOrder = function(root) {
+
+        // Add your code here
+        // To print values separated by spaces use process.stdout.write(someValue + ' ')
+        if ( root === null) {
+            return; // jika pohon kosong, maka tdk ada yg diproses
+        }
+        
+        let queue = []; // membuat antrian
+        queue.push(root); // memasukkan root ke dalam antrian
+        
+        // selaam queue tidak kosong
+        while (queue.length > 0) {
+            let node = queue.shift(); // mengambil node pertama dari antrian
+            process.stdout.write(node.data + ' '); // mencetak data node saat ini
+            
+            // jika anak kiri ada, masukkan ke dalam antrian
+            if (node.left !== null) {
+                queue.push(node.left);
+            }
+            
+            // jika anak kanan ada, masukkan ke dalam antrian
+            if (node.right !== null) {
+                queue.push(node.right);
+            }
+        }
+
+	}; // End of function levelOrder
+}; // End of function BinarySearchTree
+
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var _input = "";
+
+process.stdin.on('data', function (data) {
+    _input += data;
+});
+
+process.stdin.on('end', function () {
+    var tree = new BinarySearchTree();
+    var root = null;
+    
+    var values = _input.split('\n').map(Number);
+    
+    for (var i = 1; i < values.length; i++) {
+        root = tree.insert(root, values[i]);
+    }
+    
+    tree.levelOrder(root);
+});
